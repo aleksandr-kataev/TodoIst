60885e3474d631daec0af80a5daab78217ca9323
Aleksandr Kataev <akataev1819@gmail.com>
2020-10-04 23:52:47 +0100

    Added selectedProject/projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Content } from './components/layout/Content';
 import { Header } from './components/layout/Header';
+import {
+  ProjectsProvider,
+  SelectedProjectsProvider,
+} from './context';
 
 export const App = () => {
   return (
-    <div className='app'>
-      <Header />
-      <Content />
-    </div>
+    <SelectedProjectsProvider>
+      <ProjectsProvider>
+        <div className='app'>
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectsProvider>
   );
 };
